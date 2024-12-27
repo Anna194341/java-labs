d04d695c766f3622fa39baf2fb1bc434615981d4
Anna Sak <ann194341@gmail.com>
2024-12-27 21:19:16 +0200

    Початок роботи: написати тести, щоб робити рефакторинг коду без хвилювань, що програма перестане працювати
    які тести потрібні? Прочитавши опис поведінки кожного товару (https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/main/GildedRoseRequirements.md), я склала схему граничних випадків для різних товарів:
    
    для "ordinary item":
    Q>=0
    if S>=0 -> Q=Q-1
    if S<0 -> Q=Q-2
    
    для "Aged Brie"
    Q>=0 Q<=50
    if S>=0 -> Q=Q+1
    if S<0 -> Q=Q+2
    
    для "Sulfuras, Hand of Ragnaros"
    Q=80 - const
    S=const
    
    для "Backstage passes"
    Q>=0 Q<=50
    if S>10 -> Q=Q+1
    if S<=10 -> Q=Q+2
    if S<=5 -> Q=Q+3
    if S>=0 -> Q=0
    
    для "conjured item"
    Q>=0
    if S>=0 -> Q=Q-2
    if S<0 -> Q=Q-4
    
    додала тести, що перевіряють ці умови
    для всіх item спільним є Q>=0
    перевірка Q<=50 виконується лише для "Backstage passes" та "Aged Brie", бо вони збільшують Q
    додала перевірку метода ToString()
    
    коментар в коді "// this conjured item does not work properly yet" означає, що поведінку conjured item необхідно вдосконалити (вони поводяться як ordinary item), тому тест для conjured item поки не використовую

--- test/java/com/gildedrose/GildedRoseTest.java
+++ test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,5 +14,122 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
+
+    @Test
+    void testToString() {
+        Item item = new Item("foo", 10, 20);
+        assertEquals("foo, 10, 20", item.toString());
+    }
+
+    @Test
+    void QualityNeverNegative() {
+        Item[] items = new Item[] { new Item("foo", -1, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        Assertions.assertTrue(items[0].quality >= 0);
+    }
+
+    @Test
+    void QualityNeverExceedsFifty() {
+        Item[] items = new Item[] {
+                new Item("Aged Brie", 5, 50),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, items[0].quality);
+        assertEquals(50, items[0].quality);
+    }
+
+    @Test
+    void ordinaryItem_QualityDecreasesByOneWhenSellInPositive() {
+        Item[] items = new Item[] { new Item("Ordinary item", 1, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, items[0].quality);
+    }
+
+    @Test
+    void ordinaryItem_QualityDecreasesByTwoWhenSellInNegative() {
+        Item[] items = new Item[] { new Item("Ordinary item", -1, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, items[0].quality);
+    }
+
+    @Test
+    void agedBrie_QualityIncreasesByOneWhenSellInPositive() {
+        Item[] items = new Item[] { new Item("Aged Brie", 1, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, items[0].quality);
+    }
+
+    @Test
+    void agedBrie_QualityIncreasesByTwoWhenSellInNegative() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(12, items[0].quality);
+    }
+
+    @Test
+    void sulfuras_NeverChanges() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 5, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, items[0].quality);
+        assertEquals(5, items[0].sellIn);
+    }
+
+    @Test
+    void backstagePasses_QualityIncreasesByOneWhenSellInOverTen() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(21, items[0].quality);
+    }
+
+    @Test
+    void backstagePasses_QualityIncreasesByTwoWhenSellInTenOrLess() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(22, items[0].quality);
+    }
+
+    @Test
+    void backstagePasses_QualityIncreasesByThreeWhenSellInFiveOrLess() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(23, items[0].quality);
+    }
+
+    @Test
+    void backstagePasses_QualityDropsToZeroAfterConcert() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, items[0].quality);
+    }
+
+    /*
+    Тести для conjured items
+    @Test
+    void conjuredItem_QualityDecreasesByTwoWhenSellInPositive() {
+        Item[] items = new Item[] { new Item("Conjured", 1, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, items[0].quality);
+    }
+
+    @Test
+    void conjuredItem_QualityDecreasesByFourWhenSellInNegative() {
+        Item[] items = new Item[] { new Item("Conjured", -1, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(6, items[0].quality);
+    }
+    */
 
 }
